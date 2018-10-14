000e4909099b972006f5dc70b1b71f10d161c0a9
Chandradev <chandradev819@hotmail.com>
2018-10-14 20:41:57 +0530

    Code has been completed.

---
+++ Currency_Converter/CurChange.cs
@@ -0,0 +1,39 @@
+﻿namespace Currency_Converter
+{
+    public class CurChange
+    {
+        public int FiftyPound { get; }
+        public int TwentyPound { get; }
+        public int TenPound { get; }
+        public int FivePound { get; }
+        public int TwoPound { get; }
+        public int OnePound { get; }
+        public int FifyPence { get; }
+
+
+        public CurChange(decimal price)
+        {
+
+            FiftyPound = (int)(price / 50);
+            price %= 50;
+
+            TwentyPound = (int)(price / 20);
+            price %= 20;
+
+            TenPound = (int)(price / 10);
+            price %= 10;
+
+            FivePound = (int)(price / 5);
+            price %= 5;
+
+            TwoPound = (int)(price / 2);
+            price %= 2;
+
+            OnePound = (int)(price / 1);
+            price %= 1;
+
+            FifyPence = (int)(price / .50m);
+            price %= .50m;
+        }
+    }
+}
--- Currency_Converter/Program.cs
+++ Currency_Converter/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Currency_Converter
 {
@@ -10,6 +7,20 @@ namespace Currency_Converter
     {
         static void Main(string[] args)
         {
+            decimal GivenCurrency = 20, ProductPrice = 5.5m;
+            decimal price = (GivenCurrency - ProductPrice);
+            
+            CurChange change = new CurChange(price);
+            Console.WriteLine("Given changes are given below:");
+            Console.WriteLine($"Fifty Pound : {change.FiftyPound}");
+            Console.WriteLine($"Twenty Dollar Bills: {change.TwentyPound}");
+            Console.WriteLine($"Ten Pound : {change.TenPound}");
+            Console.WriteLine($"Five Pound : {change.FivePound}");
+            Console.WriteLine($"Two Pound: {change.TwoPound}");
+            Console.WriteLine($"One Pound: {change.OnePound}");
+            Console.WriteLine($"Fify Pence: {change.FifyPence}");
+            Console.Read();
         }
     }
+
 }
