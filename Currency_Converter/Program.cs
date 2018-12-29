f4aba4a291953e9e09a49ddaf3dffa6ce84682fe
Chandradev_Prasad <chandradev_prasad@dellteam.com>
2018-12-29 15:51:05 +0530

    Testing comment added.

--- Currency_Converter/Program.cs
+++ Currency_Converter/Program.cs
@@ -9,18 +9,19 @@ namespace Currency_Converter
         {
             try
             {
-            decimal givenCurrency = 20, productPrice = 5.5m;
-            decimal price = (givenCurrency - productPrice);
-            CurChange change = new CurChange(price);
-            Console.WriteLine("Given changes are given below:");
-            Console.WriteLine($"Fifty Pound : {change.FiftyPound}");
-            Console.WriteLine($"Twenty Dollar Bills: {change.TwentyPound}");
-            Console.WriteLine($"Ten Pound : {change.TenPound}");
-            Console.WriteLine($"Five Pound : {change.FivePound}");
-            Console.WriteLine($"Two Pound: {change.TwoPound}");
-            Console.WriteLine($"One Pound: {change.OnePound}");
-            Console.WriteLine($"Fify Pence: {change.FifyPence}");
-            Console.Read();
+                decimal givenCurrency = 20, productPrice = 5.5m;
+                decimal price = (givenCurrency - productPrice);
+                CurChange change = new CurChange(price);
+                Console.WriteLine("Given changes are given below:");
+                Console.WriteLine($"Fifty Pound : {change.FiftyPound}");
+                Console.WriteLine($"Twenty Dollar Bills: {change.TwentyPound}");
+                Console.WriteLine($"Ten Pound : {change.TenPound}");
+                Console.WriteLine("Testing");
+                Console.WriteLine($"Five Pound : {change.FivePound}");
+                Console.WriteLine($"Two Pound: {change.TwoPound}");
+                Console.WriteLine($"One Pound: {change.OnePound}");
+                Console.WriteLine($"Fify Pence: {change.FifyPence}");
+                Console.Read();
             }
             catch (Exception ex)
             {
